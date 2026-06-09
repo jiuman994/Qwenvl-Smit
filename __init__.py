83fc16d44155423e8f9961dc360648337687f513
jiuman994 <jiummmmm@163.com>
2026-06-09 22:46:55 +0800

    Add Qwen3.5 GGUF support

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+import os
+
+os.environ.setdefault("HF_DEACTIVATE_ASYNC_LOAD", "1")
+
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
