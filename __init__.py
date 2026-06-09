fd3b854d965128c6147813b0b155affde67c910a
jiuman994 <jiummmmm@163.com>
2026-06-09 23:14:29 +0800

    Remove Qwen3.5 support

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
-import os
-
-os.environ.setdefault("HF_DEACTIVATE_ASYNC_LOAD", "1")
-
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
